ce84357bbd961570e48fcc5f595e36d3c1d0d06a
tp <tp@tps-iMac.local>
2019-03-11 14:16:20 +0100

    Initial Commit

---
+++ tppersistenceTests/tppersistenceTests.swift
@@ -0,0 +1,34 @@
+//
+//  tppersistenceTests.swift
+//  tppersistenceTests
+//
+//  Created by tp on 11/03/2019.
+//  Copyright © 2019 projet. All rights reserved.
+//
+
+import XCTest
+@testable import tppersistence
+
+class tppersistenceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
