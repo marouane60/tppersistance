66b15b1060178fef74114c6104fde8fbcad37de0
tp <tp@tps-iMac.local>
2019-03-11 15:20:39 +0100

    first commit

--- tppersistence/ViewController.swift
+++ tppersistence/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import CoreData
+
 
 class ViewController: UIViewController {
 
+    let leContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let ville = Ville(context: leContext)
+        
+        ville.nom = "Orléans"
+        ville.population = 200000
+        
+        do{
+            try
+                leContext.save()
+        }
+            catch{
+                print("probleme de sauvegarde")
+            }
+        
+        let requete : NSFetchRequest<Ville> = Ville.fetchRequest()
+        
+        do {
+            let resultat = try leContext.fetch(requete)
+            print(resultat)
+        }
+        catch{
+            print("probleme")
+        }
     }
 
 
 }
-
